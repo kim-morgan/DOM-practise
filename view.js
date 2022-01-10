a9a1988c2d5f1766c717b5c1c333b5e951e92db1
Muhammad Choudhry <muhammadshahzaibc@gmail.com>
2022-01-10 16:32:17 +0000

    add paragraph

--- view.js
+++ view.js
@@ -2,7 +2,15 @@ class View {
   constructor() {
     this.mainContainerEl = document.querySelector('#main-container');
 
+
     console.log(this.mainContainerEl);
+  }
+
+  addParagraph() {
+    let p = document.createElement("p")
+    p.innerText = 'This paragraph has been dynamically added by JavaScript!'
+    this.mainContainerEl.append(p)
+
   }
 }
 
