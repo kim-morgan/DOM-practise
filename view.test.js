8c1a867c015015357a331375ec3dd5185fe289ca
Kimberley Morgan <kimberley.morgan135@gmail.com>
2022-01-10 16:45:48 +0000

    testing addParagraph

--- view.test.js
+++ view.test.js
@@ -8,9 +8,14 @@ const View = require('./view');
 describe('Page view', () => {
   it('displays 2 paragraphs', () => {
     document.body.innerHTML = fs.readFileSync('./index.html');
-
     const view = new View();
-
     expect(document.querySelectorAll('p').length).toBe(2);
   });
+
+  it('should be able to add a paragraph', () => {
+    document.body.innerHTML = fs.readFileSync('./index.html');
+    const view = new View();
+    view.addParagraph();
+    expect(document.querySelectorAll('p').length).toBe(3);
+  })
 });
